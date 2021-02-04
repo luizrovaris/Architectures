6e4453c19300f854b82e78560b94bba6aa703a90
luizrovaris <luiz_rovaris@hotmail.com>
2021-02-04 17:32:29 -0300

    Façade - Creating customer search

--- Facade/CustomerRelationship/UI/CustomerRelationship.Web/ClientApp/src/app/customer/search/customer.search.component.ts
+++ Facade/CustomerRelationship/UI/CustomerRelationship.Web/ClientApp/src/app/customer/search/customer.search.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Customer } from "../../model/Customer";
 
 @Component({
   selector: "customer-search",
@@ -6,5 +7,17 @@ import { Component } from "@angular/core";
   styleUrls: ["./customer.search.component.css"]
 })
 export class CustomerSearchComponent {
-    
+    public customers: Customer[];
+
+  constructor() {
+    this.getAllCustomers();
+  }
+
+
+  public getAllCustomers() {
+    this.customers = [
+      { id: 1, federalIdentificatin: "1234", name: "Customer 1" },
+      { id: 2, federalIdentificatin: "1235", name: "Customer 2" }];
+
+  }
 }
---
+++ Facade/CustomerRelationship/UI/CustomerRelationship.Web/ClientApp/src/app/model/Customer.ts
@@ -0,0 +1,6 @@
+export class Customer {
+  public id: number;
+  public federalIdentificatin: string;
+  public name: string;
+  //address & type
+}
