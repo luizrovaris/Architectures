50f9eb8f560337c2e0f98a7f2716f2270e22094f
Luiz Eduardo Rovaris <luiz_rovaris@hotmail.com>
2021-02-28 20:31:38 +0000

    Façade - Creating writte-off angular component.

---
+++ Facade/CustomerRelationship/UI/CustomerRelationship.Web/ClientApp/src/app/account-receivable/writteoff/account-receivable.writteoff.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "account-receivable-writteoff",
+  templateUrl: "./account-receivable.writteoff.component.html",
+  styleUrls: ["./account-receivable.writteoff.component.css"]
+})
+export class AccountReceivableWritteoffComponent {
+
+}
