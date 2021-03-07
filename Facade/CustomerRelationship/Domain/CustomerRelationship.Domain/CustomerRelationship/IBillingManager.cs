85de2458db7bee8a12cf1f185c1b80e019de6ec6
Luiz Eduardo Rovaris <luiz_rovaris@hotmail.com>
2021-03-07 23:58:48 +0000

    Façade - Adding billing manager to domain.

---
+++ Facade/CustomerRelationship/Domain/CustomerRelationship.Domain/CustomerRelationship/IBillingManager.cs
@@ -0,0 +1,9 @@
+﻿using CustomerRelationship.Infrastructure.Domain.Domain;
+using CustomerRelationship.Model.Entities;
+
+namespace CustomerRelationship.Domain.CustomerRelationship
+{
+    public interface IBillingManager : IRepositoryDomainBase<Billing>
+    {
+    }
+}
---
+++ Facade/CustomerRelationship/Domain/CustomerRelationship.Domain/CustomerRelationship/Impl/BillingManager.cs
@@ -0,0 +1,14 @@
+﻿using CustomerRelationship.Entity.DataContext;
+using CustomerRelationship.Infrastructure.Domain.Domain.Impl;
+using CustomerRelationship.Model.Entities;
+
+namespace CustomerRelationship.Domain.CustomerRelationship.Impl
+{
+    public class BillingManager : RepositoryDomainBase<Billing, CustomerRelationshipContext>, IBillingManager
+    {
+        public BillingManager(CustomerRelationshipContext context)
+               : base(context)
+        {
+        }
+    }
+}
