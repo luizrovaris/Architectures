e713f6341a94322a5ab81f6c5caa5ba41a215b3e
Luiz Eduardo Rovaris <luiz_rovaris@hotmail.com>
2021-03-09 18:53:43 +0000

    Façade - Adding angular customer service.

---
+++ Facade/CustomerRelationship/UI/CustomerRelationship.Web/ClientApp/src/app/services/customer/customer.service.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.CustomerService = void 0;
+var CustomerService = /** @class */ (function () {
+    function CustomerService() {
+    }
+    return CustomerService;
+}());
+exports.CustomerService = CustomerService;
+//# sourceMappingURL=customer.service.js.map
