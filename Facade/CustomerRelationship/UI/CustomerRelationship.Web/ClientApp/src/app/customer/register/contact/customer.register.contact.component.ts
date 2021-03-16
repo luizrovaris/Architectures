17dca484dc80175eefe791426157917806061c0f
Luiz Eduardo Rovaris <luiz_rovaris@hotmail.com>
2021-03-16 22:55:36 +0000

    Façade - Angular - Creating method to edit contacts.

--- Facade/CustomerRelationship/UI/CustomerRelationship.Web/ClientApp/src/app/customer/register/contact/customer.register.contact.component.ts
+++ Facade/CustomerRelationship/UI/CustomerRelationship.Web/ClientApp/src/app/customer/register/contact/customer.register.contact.component.ts
@@ -6,8 +6,10 @@ import { Contact } from "../../../model/contact";
   templateUrl: 'customer.register.contact.component.html',
   styleUrls: ['customer.register.contact.component.css']
 })
-export class CustomerRegisterContactComponent implements OnInit {  
+export class CustomerRegisterContactComponent implements OnInit {
+  public contactMessage: string;
   public contact: Contact;
+  public isEditing: boolean;
   @Input() contacts: Contact[];
   @Output() newContactsEvent = new EventEmitter<Contact[]>();
 
@@ -15,10 +17,40 @@ export class CustomerRegisterContactComponent implements OnInit {
   }
   ngOnInit(): void {
     this.contact = new Contact();
+    this.isEditing = false;
   }
 
   public addContact() {
-    this.contacts.push(this.contact);
-    this.newContactsEvent.emit(this.contacts);
+    this.contactMessage = "";
+
+    if (this.isEditing) {
+      this.contact = new Contact();
+    }
+    else {
+      let currentContact = this.contacts.find(x => x.name === this.contact.name);
+      
+      if (currentContact) {
+        this.contactMessage = "Already exists a contatct with the same name";
+      }
+      else {
+        this.contacts.push(this.contact);
+        this.newContactsEvent.emit(this.contacts);
+        this.contact = new Contact();
+      }
+    }
+
+    this.isEditing = false;
+  }
+
+  public editContact(currentContact: Contact) {
+    this.isEditing = true;
+    this.contact = this.contacts.find(x => x.name === currentContact.name);
+  }
+
+  public removeContact(currentContact: Contact) {
+    const index = this.contacts.indexOf(currentContact, 0);
+    if (index > -1) {
+      this.contacts.splice(index, 1);
+    }
   }
 }
