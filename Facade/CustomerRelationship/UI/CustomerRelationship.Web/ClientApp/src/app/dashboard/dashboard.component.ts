dae0703d641e14c474544a849c4c033fe4a4c602
luizrovaris <luiz_rovaris@hotmail.com>
2021-03-29 20:44:57 +0100

    Façade - Changing app layout.

--- Facade/CustomerRelationship/UI/CustomerRelationship.Web/ClientApp/src/app/dashboard/dashboard.component.ts
+++ Facade/CustomerRelationship/UI/CustomerRelationship.Web/ClientApp/src/app/dashboard/dashboard.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
   selector: 'dashboard-cr',
-  templateUrl: './dashboard.component.html'
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent {
 
