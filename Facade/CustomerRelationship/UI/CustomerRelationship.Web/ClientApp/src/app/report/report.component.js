55f3f87fd0c808c37d0ca76ce997a3d8a136cb27
luizrovaris <luiz_rovaris@hotmail.com>
2021-04-02 16:16:31 +0100

    Façade - Adding report component.

---
+++ Facade/CustomerRelationship/UI/CustomerRelationship.Web/ClientApp/src/app/report/report.component.js
@@ -0,0 +1,10 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.ReportComponent = void 0;
+var ReportComponent = /** @class */ (function () {
+    function ReportComponent() {
+    }
+    return ReportComponent;
+}());
+exports.ReportComponent = ReportComponent;
+//# sourceMappingURL=report.component.js.map
