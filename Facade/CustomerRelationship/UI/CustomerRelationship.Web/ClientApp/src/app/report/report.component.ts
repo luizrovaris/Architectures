f93f707c415d15f9107013115baaad7ab71b4422
luizrovaris <luiz_rovaris@hotmail.com>
2021-04-09 19:42:50 +0100

    Façade - Creating report layout.

--- Facade/CustomerRelationship/UI/CustomerRelationship.Web/ClientApp/src/app/report/report.component.ts
+++ Facade/CustomerRelationship/UI/CustomerRelationship.Web/ClientApp/src/app/report/report.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { Customer } from "../model/Customer";
 
 @Component({
   selector: "report",
@@ -6,5 +7,6 @@ import { Component } from "@angular/core";
   styleUrls: ["./report.component.css"]
 })
 export class ReportComponent {
+  public customers: Customer[];
 
 }
